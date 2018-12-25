330d4bed1359c41b86786f6e069d742ae2e51a87
Jurek Völp <jurvoel@gmx.de>
2018-12-25 23:48:33 +0100

    added CoordinateSystem class

---
+++ Mandelbrotmenge/CoordinateSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Mandelbrotmenge
+{
+	static class CoordinateSystem
+	{
+		public static Point CenterPoint;
+		public static Size Size;
+		public static double xMin;
+		public static double xMax;
+		public static double yMin;
+		public static double yMax;
+	}
+}
