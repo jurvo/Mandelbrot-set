8154955e861c8ea5d75d6c013c3a5930c6ed284b
Jurek Völp <jurvoel@gmx.de>
2019-01-12 14:09:04 +0100

    added flags for control, added crosshair

--- Mandelbrotmenge/main.cs
+++ Mandelbrotmenge/main.cs
@@ -14,36 +14,54 @@ namespace Mandelbrotmenge
 	{
 		int numberOfMaxIterations;
 		double screenRatio;
+		bool drawCoord;
+		bool drawInfo;
+		bool drawCoordInfo;
+		bool drawPerfInfo;
+		bool drawCenterCoordInfo;
+		bool drawCrosshair;
+
+		Bitmap b;
 
 		public main()
 		{
 			InitializeComponent();
 			MouseWheel += Main_MouseWheel;
 
-			Size = Screen.PrimaryScreen.Bounds.Size;
-			//Size = new Size(1000, 500);
+			Size = Screen.PrimaryScreen.Bounds.Size; //set fullscreen
+
+			#region Init of CoordinateSystem
 			screenRatio = ClientSize.Width / (double)ClientSize.Height;
-
 			CoordinateSystem.Size = ClientSize;
-
 			CoordinateSystem.yMin = -2;
 			CoordinateSystem.yMax = 2;
 			CoordinateSystem.xMin = CoordinateSystem.yMin * screenRatio;
 			CoordinateSystem.xMax = CoordinateSystem.yMax * screenRatio;
 			CoordinateSystem.calcOrigin();
-
 			CoordinateSystem.Pen = new Pen(Color.Red, 1);
-			numberOfMaxIterations = 100;
+			#endregion
+
+			#region Settings
+			numberOfMaxIterations = 100; //Number of max Iteration to proof that c is bounded
+			drawCoord = true; //flag to control if the axis is drawn
+			drawInfo = true; //flag to control if the informations are drawn, if false, non of the infos  is drawn, wheter or not the flags are true
+			drawCoordInfo = true; //flag to control if values like xMin and xMax are drawn
+			drawPerfInfo = true; //flag to control if the delta time is drawn
+			drawCenterCoordInfo = true; //flag to control if the centerscreen coordinate is drawn
+			drawCrosshair = true; //flag to control if the crosshair is drawn
+			#endregion
 		}
 
 		protected override void OnPaint(PaintEventArgs e)
 		{
 			base.OnPaint(e);
 			DateTime t = DateTime.Now;
+
+			#region set drawing
 			int numberOfIterations;
 			ComplexNumber c;
 			ComplexNumber z;
-			Bitmap b = new Bitmap(ClientSize.Width, ClientSize.Height);
+			b = new Bitmap(ClientSize.Width, ClientSize.Height);
 
 			for (double i = 0; i < ClientSize.Width; i++)
 			{
@@ -63,39 +81,57 @@ namespace Mandelbrotmenge
 
 			if (b != null)
 				e.Graphics.DrawImage(b, new PointF(0, 0));
+			#endregion
 
 			#region Coordinate System drawing
-
-			//x-Achse
-			if (CoordinateSystem.Origin.Y > 0 && CoordinateSystem.Origin.Y < ClientSize.Height)
+			if (drawCoord)
 			{
-				e.Graphics.DrawLine(CoordinateSystem.Pen, new Point(0, CoordinateSystem.Origin.Y), new Point(ClientSize.Width, CoordinateSystem.Origin.Y));
-				for (int i = 1; i < CoordinateSystem.xMax; i++)
+				//x-Achse
+				if (CoordinateSystem.Origin.Y > 0 && CoordinateSystem.Origin.Y < ClientSize.Height)
 				{
-					e.Graphics.DrawLine(CoordinateSystem.Pen, new Point((int)(CoordinateSystem.Origin.X + i * CoordinateSystem.Size.Width / (CoordinateSystem.xMax - CoordinateSystem.xMin)), CoordinateSystem.Origin.Y - 5), new Point((int)(CoordinateSystem.Origin.X + i * CoordinateSystem.Size.Width / (CoordinateSystem.xMax - CoordinateSystem.xMin)), CoordinateSystem.Origin.Y + 5));
+					e.Graphics.DrawLine(CoordinateSystem.Pen, new Point(0, CoordinateSystem.Origin.Y), new Point(ClientSize.Width, CoordinateSystem.Origin.Y));
+					for (int i = 1; i < CoordinateSystem.xMax; i++)
+					{
+						e.Graphics.DrawLine(CoordinateSystem.Pen, new Point((int)(CoordinateSystem.Origin.X + i * CoordinateSystem.Size.Width / (CoordinateSystem.xMax - CoordinateSystem.xMin)), CoordinateSystem.Origin.Y - 5), new Point((int)(CoordinateSystem.Origin.X + i * CoordinateSystem.Size.Width / (CoordinateSystem.xMax - CoordinateSystem.xMin)), CoordinateSystem.Origin.Y + 5));
+					}
+					for (int i = -1; i > CoordinateSystem.xMin; i--)
+					{
+						e.Graphics.DrawLine(CoordinateSystem.Pen, (float)(CoordinateSystem.Origin.X + i * CoordinateSystem.Size.Width / (CoordinateSystem.xMax - CoordinateSystem.xMin)), CoordinateSystem.Origin.Y - 5, (float)(CoordinateSystem.Origin.X + i * CoordinateSystem.Size.Width / (CoordinateSystem.xMax - CoordinateSystem.xMin)), CoordinateSystem.Origin.Y + 5);
+					}
 				}
-				for (int i = -1; i > CoordinateSystem.xMin; i--)
+				//y-Achse
+				if (CoordinateSystem.Origin.X > 0 && CoordinateSystem.Origin.X < ClientSize.Width)
 				{
-					e.Graphics.DrawLine(CoordinateSystem.Pen, new Point((int)(CoordinateSystem.Origin.X + i * CoordinateSystem.Size.Width / (CoordinateSystem.xMax - CoordinateSystem.xMin)), CoordinateSystem.Origin.Y - 5), new Point((int)(CoordinateSystem.Origin.X + i * CoordinateSystem.Size.Width / (CoordinateSystem.xMax - CoordinateSystem.xMin)), CoordinateSystem.Origin.Y + 5));
-				}
-			}
-			//y-Achse
-			if (CoordinateSystem.Origin.X > 0 && CoordinateSystem.Origin.X < ClientSize.Width)
-			{
-				e.Graphics.DrawLine(CoordinateSystem.Pen, new Point(CoordinateSystem.Origin.X, 0), new Point(CoordinateSystem.Origin.X, ClientSize.Height));
-				for (int i = 1; i < CoordinateSystem.yMax; i++)
-				{
-					e.Graphics.DrawLine(CoordinateSystem.Pen, new Point(CoordinateSystem.Origin.X - 5, (int)(CoordinateSystem.Origin.Y + i * CoordinateSystem.Size.Height / (CoordinateSystem.yMax - CoordinateSystem.yMin))), new Point(CoordinateSystem.Origin.X + 5, (int)(CoordinateSystem.Origin.Y + i * CoordinateSystem.Size.Height / (CoordinateSystem.yMax - CoordinateSystem.yMin))));
-				}
-				for (int i = -1; i > CoordinateSystem.yMin; i--)
-				{
-					e.Graphics.DrawLine(CoordinateSystem.Pen, new Point(CoordinateSystem.Origin.X - 5, (int)(CoordinateSystem.Origin.Y + i * CoordinateSystem.Size.Height / (CoordinateSystem.yMax - CoordinateSystem.yMin))), new Point(CoordinateSystem.Origin.X + 5, (int)(CoordinateSystem.Origin.Y + i * CoordinateSystem.Size.Height / (CoordinateSystem.yMax - CoordinateSystem.yMin))));
+					e.Graphics.DrawLine(CoordinateSystem.Pen, new Point(CoordinateSystem.Origin.X, 0), new Point(CoordinateSystem.Origin.X, ClientSize.Height));
+					for (int i = 1; i < CoordinateSystem.yMax; i++)
+					{
+						e.Graphics.DrawLine(CoordinateSystem.Pen, CoordinateSystem.Origin.X - 5, (float)(CoordinateSystem.Origin.Y + i * CoordinateSystem.Size.Height / (CoordinateSystem.yMax - CoordinateSystem.yMin)), CoordinateSystem.Origin.X + 5, (float)(CoordinateSystem.Origin.Y + i * CoordinateSystem.Size.Height / (CoordinateSystem.yMax - CoordinateSystem.yMin)));
+					}
+					for (int i = -1; i > CoordinateSystem.yMin; i--)
+					{
+						e.Graphics.DrawLine(CoordinateSystem.Pen, CoordinateSystem.Origin.X - 5, (float)(CoordinateSystem.Origin.Y + i * CoordinateSystem.Size.Height / (CoordinateSystem.yMax - CoordinateSystem.yMin)), CoordinateSystem.Origin.X + 5, (float)(CoordinateSystem.Origin.Y + i * CoordinateSystem.Size.Height / (CoordinateSystem.yMax - CoordinateSystem.yMin)));
+					}
 				}
 			}
 			#endregion
 
-			string s = "xMin: " + CoordinateSystem.xMin + "\n" + "xMax: " + CoordinateSystem.xMax + "\n" + "yMin: " + CoordinateSystem.yMin + "\n" + "yMax: " + CoordinateSystem.yMax + "\n" + ScreenToCoordinate(new PointF(ClientSize.Width / 2, ClientSize.Height / 2)).ToString();
-			e.Graphics.DrawString(s, Font, Brushes.Red, new PointF(0, 0));
+			#region info drawing
+			string s = "";
+			if (drawCoordInfo)
+				s += "xMin: " + CoordinateSystem.xMin + "\nxMax: " + CoordinateSystem.xMax + "\nyMin: " + CoordinateSystem.yMin + "\nyMax: " + CoordinateSystem.yMax + "\n";
+			if (drawPerfInfo)
+				s += (DateTime.Now - t).ToString()+ "\n";
+			if (drawCenterCoordInfo)
+				s += ScreenToCoordinate(new PointF(ClientSize.Width / 2, ClientSize.Height / 2)).ToString() + "\n";
+			if (drawInfo)
+				e.Graphics.DrawString(s, Font, Brushes.Red, new PointF(0, 0));
+
+			if (drawCrosshair)
+			{
+				e.Graphics.DrawLine(CoordinateSystem.Pen, ClientSize.Width / 2, ClientSize.Height / 2 - 5, ClientSize.Width / 2, ClientSize.Height / 2 + 5);
+				e.Graphics.DrawLine(CoordinateSystem.Pen, ClientSize.Width / 2 - 5, ClientSize.Height / 2, ClientSize.Width / 2 + 5, ClientSize.Height / 2 );
+			}
+			#endregion
 		}
 
 		private void main_KeyDown(object sender, KeyEventArgs e)
